1b7998b5d25f7476beefa3b13763fe222030aeb5
Maksim Kostromin <daggerok@gmail.com>
2017-01-16 04:56:09 +0200

    add custom sender

--- src/main/java/daggerok/messaging/rabbit/rest/MessagingService.java
+++ src/main/java/daggerok/messaging/rabbit/rest/MessagingService.java
@@ -10,12 +10,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/message/send")
 public class MessagingService {
 
     final Publisher publisher;
@@ -25,7 +31,7 @@ public class MessagingService {
     final WorkerQueueSender workerQueueSender;
     final RpcSenderReceiver rpcSenderReceiver;
 
-    @RequestMapping("/message/send")
+    @RequestMapping("")
     public ResponseEntity<String> send(final String message) {
 
         log.info("processing new message: {}", message);
@@ -38,6 +44,54 @@ public class MessagingService {
             topicSender.send(message);
             rpcSenderReceiver.send(message);
         }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping("/to/{dst}")
+    public ResponseEntity<String> sendTo(final @PathVariable Optional<String> dst, @RequestParam final String message) {
+
+        log.info("processing message {} to: {}", message, dst);
+
+        if (isNull(message) || "".equals(message)) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("message is required");
+        }
+
+        Optional.ofNullable(dst).ifPresent(d -> {
+
+            switch (d.get()) {
+
+                case "simpleSender":
+                    simpleSender.send(message);
+                    break;
+
+                case "workerQueueSender":
+                    workerQueueSender.send(message);
+                    break;
+
+                case "publisher":
+                    publisher.send(message);
+                    break;
+
+                case "routingSender":
+                    routingSender.send(message);
+                    break;
+
+                case "topicSender":
+                    topicSender.send(message);
+                    break;
+
+                case "rpcSenderReceiver":
+                    rpcSenderReceiver.send(message);
+                    break;
+
+                default:
+                    log.info("no receiver found for {}", d.get());
+                    send(message);
+            }
+        });
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
